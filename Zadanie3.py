8bec8c7dcb57403a2f1aa9c230932f9c3e1c1417
SalaD2000 <58978160+SalaD2000@users.noreply.github.com>
2020-04-22 23:52:36 +0200

    Add files via upload

---
+++ Zadanie3.py
@@ -0,0 +1,30 @@
+from math import *
+import sys
+
+class Kwadrat:
+
+    def __init__(self, x):
+        self.x = x
+        self.y = x
+    def __add__(self,other):
+        return self.x + other.x
+    def __ge__(self, other):
+            return self.x >= other.x
+    def __gt__(self, other):
+            return self.x > other.x
+    def __le__(self, other):
+        return self.x <= other.x
+    def __lt__(self, other):
+            return self.x < other.x
+
+
+kw1 = Kwadrat(50)
+kw2= Kwadrat(5)
+kw3= kw1 >= kw2
+kw4= kw1 > kw2
+kw5= kw1 <= kw2
+kw6= kw1 < kw2
+print(kw3)
+print(kw4)
+print(kw5)
+print(kw6)
